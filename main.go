7177f863eb4022410c5b626597d4df14b69b9186
Alexander F. Rødseth <rodseth@gmail.com>
2020-05-03 11:40:46 +0200

    One step closer

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"strconv"
@@ -12,74 +13,133 @@ import (
 
 type LanguageServer struct {
 	executable            string
-	inBuf, outBuf, errBuf bytes.Buffer
+	bufIn, bufOut, bufErr bytes.Buffer
 	cmd                   *exec.Cmd
 	running               bool
+	tmpIn                 io.Reader
+	tmpOut, tmpErr        io.Writer
 }
 
 func New() *LanguageServer {
 	var ls LanguageServer
 	ls.executable = "gopls"
 	ls.cmd = exec.Command(ls.executable)
-	ls.cmd.Stdin = &ls.inBuf
-	ls.cmd.Stdout = &ls.outBuf
-	ls.cmd.Stderr = &ls.errBuf
+	ls.tmpIn = ls.cmd.Stdin
+	ls.cmd.Stdin = &ls.bufIn
+	ls.tmpOut = ls.cmd.Stdout
+	ls.cmd.Stdout = &ls.bufOut
+	ls.tmpOut = ls.cmd.Stderr
+	ls.cmd.Stderr = &ls.bufErr
 	return &ls
 }
 
+// Input will send a string directly to the language server (and start it if needed).
+// No headers are added. The resulting stdout and stderr are returned as a string.
 func (ls *LanguageServer) Input(s string) (string, error) {
 	if !ls.running {
-		ls.inBuf.WriteString(s + "\r")
+		ls.bufIn.WriteString(s + "\r")
 		if err := ls.cmd.Start(); err != nil {
 			return "", err
 		}
 		ls.running = true
 		if err := ls.cmd.Wait(); err != nil {
-			output := strings.TrimSpace(ls.outBuf.String() + ls.errBuf.String())
+			output := strings.TrimSpace(ls.bufOut.String() + ls.bufErr.String())
 			if output != "" {
 				return "", errors.New(output)
 			}
 			return "", err
 		}
-		return ls.outBuf.String() + ls.errBuf.String(), nil
+		return ls.bufErr.String() + ls.bufOut.String(), nil
 	} else {
-		ls.inBuf.WriteString(s + "\r")
+		ls.bufIn.WriteString(s + "\r")
 		if err := ls.cmd.Wait(); err != nil {
-			output := strings.TrimSpace(ls.outBuf.String() + ls.errBuf.String())
+			output := strings.TrimSpace(ls.bufOut.String() + ls.bufErr.String())
 			if output != "" {
 				return "", errors.New(output)
 			}
 			return "", err
 		}
-		return ls.outBuf.String() + ls.errBuf.String(), nil
+		return ls.bufErr.String() + ls.bufOut.String(), nil
 	}
 }
 
-func (ls *LanguageServer) Request(msg string) (string, error) {
+// Request will take a JSON message and pass it to the running language server, with the appropriate headers
+func (ls *LanguageServer) Request(msg string, verbose bool) (string, error) {
 	var sb strings.Builder
-	sb.WriteString("Content-Length:" + strconv.Itoa(len(msg)) + "\n")
-	//sb.WriteString("Content-Type: application/vscode-jsonrpsc;charset=utf-8\n")
-	sb.WriteString("\n")
+	sb.WriteString("Content-Length: " + strconv.Itoa(len(msg)) + "\n")
+	sb.WriteString("Content-Type: application/vscode-jsonrpsc; charset=utf-8\n") // Optional
+	sb.WriteString("\n")                                                         // Blank line
 	sb.WriteString(msg)
 	sb.WriteString("\n")
-	fmt.Println("REQUEST:\n" + sb.String())
+	if verbose {
+		fmt.Println(sb.String())
+	}
+	// The protocol is written by Microsoft, so of course there are DOS line endings
 	return ls.Input(strings.Replace(sb.String(), "\n", "\r\n", -1))
+}
+
+// Close will close the in/out/err buffers and wait for the process to complete
+func (ls *LanguageServer) Close() error {
+	if ls.cmd != nil {
+		ls.cmd.Stdin = ls.tmpIn
+		ls.cmd.Stdout = ls.tmpOut
+		ls.cmd.Stderr = ls.tmpErr
+		ls.cmd.Wait()
+		ls.cmd = nil
+		return nil
+	}
+	return errors.New("No running process")
 }
 
 func main() {
 	ls := New()
+	defer ls.Close()
+
 	output, err := ls.Request(`{
   "jsonrpc": "2.0",
   "id": 1,
   "method": "textDocument/didOpen",
   "params": {}
-}`)
+}`, true)
 	if err != nil {
 		if output != "" {
 			fmt.Printf("output: %s\n", output)
 		}
 		fmt.Fprintf(os.Stderr, "error: %s\n", err)
-		return
+		// return
 	}
 	fmt.Println("OUT:\n" + output)
+
+	output, err = ls.Request(`{
+  "jsonrpc": "2.0",
+  "id": 1,
+  "method": "textDocument/didOpen",
+  "params": {}
+}`, true)
+	if err != nil {
+		if output != "" {
+			fmt.Printf("output: %s\n", output)
+		}
+		fmt.Fprintf(os.Stderr, "error: %s\n", err)
+		// return
+	}
+	fmt.Println("OUT:\n" + output)
+
+	ls.Close()
+
+	output, err = ls.Request(`{
+  "jsonrpc": "2.0",
+  "id": 1,
+  "method": "textDocument/didOpen",
+  "params": {}
+}`, true)
+	if err != nil {
+		if output != "" {
+			fmt.Printf("output: %s\n", output)
+		}
+		fmt.Fprintf(os.Stderr, "error: %s\n", err)
+		// return
+	}
+	fmt.Println("OUT:\n" + output)
+
 }
